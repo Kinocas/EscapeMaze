e05ecc6053f7f1ef4982b04eb45d123a52589d78
Kinocas <717koushi717@gmail.com>
2021-09-18 12:19:07 +0900

    キャラクター操作の実装

---
+++ Assets/EscapeMaze/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 3;
+    public float jumpPower = 3;
+
+    float vx = 0;
+    float vz = 0;
+
+    private Vector3 _moveVelocity;
+    private CharacterController _characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _moveVelocity.x = Input.GetAxis("Horizontal") * moveSpeed;
+        _moveVelocity.z = Input.GetAxis("Vertical") * moveSpeed;
+        this.transform.LookAt(this.transform.position+new Vector3(_moveVelocity.x,0,_moveVelocity.z));
+        _characterController.Move(_moveVelocity * Time.deltaTime);
+
+    }
+}
