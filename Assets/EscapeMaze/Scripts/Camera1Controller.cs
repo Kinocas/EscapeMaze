adff6c678a5c05c9ee1bc966fbc399bfd9d4c05d
Kinocas <717koushi717@gmail.com>
2021-09-21 17:17:46 +0900

    カメラ視点が壁を貫通しないように修正

--- Assets/EscapeMaze/Scripts/Camera1Controller.cs
+++ Assets/EscapeMaze/Scripts/Camera1Controller.cs
@@ -7,29 +7,55 @@ public class Camera1Controller : MonoBehaviour
     private GameObject mainCamera;
     private GameObject playerObject;
     private Vector3 playerPos;
+    private Vector3 playerBasePos;
+    private Vector3 cameraPos;
+    private Vector3 cameraForward;
+    float dis;
+
+    //　カメラのキャラクターからの相対値を指定
+    private Vector3 basePos = new Vector3(0f, 1.5f, 0);
 
     public float rotateSpeed = 5.0f;
+
+    RaycastHit hit;
 
     // Start is called before the first frame update
     void Start()
     {
         mainCamera = Camera.main.gameObject;
         playerObject = GameObject.Find("unitychan");
+
         playerPos = playerObject.transform.position;
+        playerBasePos = playerPos + basePos;
+        cameraPos = transform.position;
+        dis = Vector3.Distance(playerBasePos, cameraPos);
     }
 
     // Update is called once per frame
     void Update()
-    {
-        transform.position += playerObject.transform.position - playerPos;
+    { 
+        cameraForward = transform.forward;
+        transform.position += ((playerObject.transform.position - playerPos)
+            - cameraForward * (dis- Vector3.Distance(playerBasePos, cameraPos))) ;
+        
         playerPos = playerObject.transform.position;
+        playerBasePos = playerPos + basePos;
+        cameraPos = transform.position;
+
         rotateCamera();
+
+        //　キャラクターとカメラの間に障害物があったら障害物の位置にカメラを移動させる
+        if (Physics.Linecast(playerBasePos, transform.position, out hit))
+        {
+            transform.position = hit.point;
+        }
+
     }
 
     private void rotateCamera()
     {
         Vector3 angle = new Vector3(Input.GetAxis("Mouse X") * rotateSpeed, -Input.GetAxis("Mouse Y") * rotateSpeed, 0);
-        mainCamera.transform.RotateAround(playerObject.transform.position, Vector3.up, angle.x);
-        mainCamera.transform.RotateAround(playerObject.transform.position, transform.right, angle.y);
+        mainCamera.transform.RotateAround((playerObject.transform.position + basePos), Vector3.up, angle.x);
+        mainCamera.transform.RotateAround((playerObject.transform.position + basePos), transform.right, angle.y);
     }
 }
