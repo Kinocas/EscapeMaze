10286c6f5fc487b91179133e37535add048e9144
Kinocas <717koushi717@gmail.com>
2021-09-22 16:12:51 +0900

    敵の徘徊実装

--- Assets/EscapeMaze/Scripts/EnemyMove_Wander.cs
+++ Assets/EscapeMaze/Scripts/EnemyMove_Wander.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class EnemyMove_Wandering : MonoBehaviour
+public class EnemyMove_Wander : MonoBehaviour
 {
+    private NavMeshAgent agent;
+    public float wanderRange = 10.0f;
+
+    //設定した待機時間
+    [SerializeField] float waitTime = 2;
+    //待機時間を数える
+    [SerializeField] float time = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        //agent.autoBraking = false;
+        SetDestination();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //経路探索の準備ができておらず
+        //目標地点までの距離が0.5m未満ならNavMeshAgentを止める
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            StopHere();
+        }
+    }
+
+    void SetDestination()
+    {
+        agent.isStopped = false;
+        Vector3 randomPos = new Vector3(Random.Range(-wanderRange, wanderRange), 0, Random.Range(-wanderRange, wanderRange));
+        agent.destination = randomPos;
+    }
+
+    void StopHere()
+    {
+        //NavMeshAgentを止める
+        agent.isStopped = true;
+        //待ち時間を数える
+        time += Time.deltaTime;
+
+        //待ち時間が設定された数値を超えると発動
+        if (time > waitTime)
+        {
+            //目標地点を設定し直す
+            SetDestination();
+            time = 0;
+        }
     }
 }
