7ae7825c956c071f43b5717053c80f9c912f7ca4
Kinocas <717koushi717@gmail.com>
2021-09-23 14:23:23 +0900

    クリア画面の修正

--- Assets/EscapeMaze/Scripts/ClearTextAnimator.cs
+++ Assets/EscapeMaze/Scripts/ClearTextAnimator.cs
@@ -9,11 +9,16 @@ public class ClearTextAnimator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.DOScale(new Vector3(2f, 2f, 2f), 3f)
+        transform.DOScale(new Vector3(0f, 0f, 1f),0f)
             .OnComplete(() =>
             {
-                transform.DOPunchScale(new Vector3(1.1f, 1.1f, 1.1f), 1f);
+                transform.DOScale(new Vector3(1f, 1f, 1f), 3f)
+                    .OnComplete(() =>
+                    {
+                         transform.DOPunchScale(new Vector3(1.1f, 1.1f, 1.1f), 1f);
+                    });
             });
+        
     }
 
     // Update is called once per frame
